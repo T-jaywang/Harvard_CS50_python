21c3c4e7bc5a8c47b831ea89777814bbe32d0c5d
T-jaywang <jaywang811@gmail.com>
2025-04-20 13:32:28 +0800

    first commit

--- 1.hello.py
+++ 1.hello.py
@@ -3,7 +3,7 @@ def main():
     name = input ("What is your name? ").strip().title()
     hello(name)
 
-def hello(to):
+def hello(to = "world"): #default output "Hello, world"
     print(f"Hello, {to}")
 
 
@@ -11,6 +11,6 @@ def hello(to):
 def hello_to_friend():
     print("hellow \"friend\"") #backslash actually represents an escape character
 
-hello_to_friend()
+main()
 
 
