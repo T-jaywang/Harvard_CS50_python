c8a86678b8c0e931b73ba0f8611f5f5d1156c4e1
T-jaywang <138895256+T-jaywang@users.noreply.github.com>
2025-04-24 13:38:52 +0000

    first commit

--- zb_student.py
+++ zb_student.py
@@ -41,16 +41,123 @@
 #     main()
 
 # THE FOURTH VERSION
+#def main():
+#    student = get_student()
+#    if student["name"] == "Padma":
+#        student["house"] = "Ravenclaw"
+#    print(f"{student['name']} from {student['house']}")
+#
+#def get_student():
+#    name = input("Name: ")
+#    house = input("House: ")
+#    return {"name":name, "house": house}
+#
+#if __name__ == "__main__":
+#    main()
+
+#THE FIFTH VERSION
+#class Student:
+#    def __init__(self,name,house,patronus):
+#        if not name:
+#            raise ValueError("Missing name")
+#        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+#            raise ValueError("Invalid house")
+#        self.name = name
+#        self.house = house
+#        self.patronus = patronus
+#
+#    def __str__(self):
+#        return f"{self.name} from {self.house}"
+#    
+#    def charm(self):
+#        match self.patronus:
+#            case "Stag":
+#                return"🐴"
+#            case "Otter":
+#                return"👞"
+#            case "Jack Russell terrier":
+#                return"🍕"
+#            case _:
+#                return"🐷"
+
+    
+#def main():
+    #student = get_student()
+    #print(f"{student.name} from {student.house}")
+
+#def get_student():
+#    student = Student()
+#    student.name = input("Name: ")
+#    student.house = input("House: ")
+#    return student
+
+#if __name__ == "__main__":
+#    main()
+
+#THE SIXTH VERSION
+#class Student:
+#    def __init__(self, name, house):
+#        if not name:
+#            raise ValueError("Missing name")
+#        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+#            raise ValueError("Invalid house")
+#        self.name = name
+#        self.house = house
+        
+#    def __str__(self):
+#        return f"{self.name} from {self.house}"
+    
+#def main():
+#    student = get_student()
+#    print(student)
+
+# def get_student():
+#    name = input("Name: ")
+#    house = input("House: ")
+#    return Student(name,house)
+
+# if __name__ == "__main__":
+    #main()
+
+#THE SEVENTH VERSION
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+    
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor" , "Hufflepuff" , "Ravenclaw" , "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return {"name":name, "house": house}
+    return Student(name,house)
 
 if __name__ == "__main__":
     main()
---
+++ zc_hat.py
@@ -0,0 +1,25 @@
+#THE FIRST VERSION
+#import random
+
+
+#class Hat:
+#    def __init__(self):
+#        self.houses = ["Gryffindor", "Hufflepuff", "Revenclaw", "Slytherin"]
+    
+#    def sort(self, name):
+#        print(name,"is in", random.choice(self.houses))
+
+#hat = Hat()
+#hat.sort("Harry")
+
+import random
+
+class Hat:
+
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+
+Hat.sort("Harry")
