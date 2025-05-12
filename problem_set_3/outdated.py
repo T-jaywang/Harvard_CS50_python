4a90ae699ca98f1af508969efcf4e2d52a7729e2
王彥傑 <jaywang811@wangyanjiedeMacBook-Air.local>
2025-05-12 23:11:33 +0800

    first commit

---
+++ problem_set_3/outdated.py
@@ -0,0 +1,21 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+def main():
+    ...
+
+
+def get_date():
+    ...
