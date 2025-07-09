39a17b0b2ba127bf5d5e1115cda46ca42782cfe2
王彥傑 <jaywang811@wangyanjiedeMacBook-Air.local>
2025-07-09 21:34:20 +0800

    first commit

--- python_corse/zc_hat.py
+++ python_corse/zc_hat.py
@@ -1,26 +1,26 @@
 #THE FIRST VERSION
-#import random
-
-
-#class Hat:
-#    def __init__(self):
-#        self.houses = ["Gryffindor", "Hufflepuff", "Revenclaw", "Slytherin"]
-    
-#    def sort(self, name):
-#        print(name,"is in", random.choice(self.houses))
-
-#hat = Hat()
-#hat.sort("Harry")
-
-# THE SECOND VERSION
 import random
 
+
 class Hat:
+   def __init__(self):
+       self.houses = ["Gryffindor", "Hufflepuff", "Revenclaw", "Slytherin"]
+    
+   def sort(self, name):
+       print(name,"is in", random.choice(self.houses))
 
-    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+hat = Hat()
+hat.sort("Harry")
 
-    @classmethod
-    def sort(cls, name):
-        print(name, "is in", random.choice(cls.houses))
+# THE SECOND VERSION
+# import random
 
-Hat.sort("Harry")
+# class Hat:
+
+#     houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+#     @classmethod
+#     def sort(cls, name):
+#         print(name, "is in", random.choice(cls.houses))
+
+# Hat.sort("Harry")
